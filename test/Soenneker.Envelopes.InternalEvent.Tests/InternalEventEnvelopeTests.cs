9ca83d64d4c95cd2b970b8daa90e92239a45e0f4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:48 -0500

    TUnit conversion

--- test/Soenneker.Envelopes.InternalEvent.Tests/InternalEventEnvelopeTests.cs
+++ test/Soenneker.Envelopes.InternalEvent.Tests/InternalEventEnvelopeTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Envelopes.InternalEvent.Tests;
 
 public sealed class InternalEventEnvelopeTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
